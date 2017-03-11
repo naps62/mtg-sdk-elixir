987cb3d703ce11ac1882b306cafdec977787d645
Miguel Palhas <mpalhas@gmail.com>
2017-03-11 14:08:43 +0000

    First commit

---
+++ lib/mtg.ex
@@ -0,0 +1,18 @@
+defmodule MTG do
+  @moduledoc """
+  Documentation for MTG.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MTG.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mtg_test.exs
@@ -0,0 +1,8 @@
+defmodule MTGTest do
+  use ExUnit.Case
+  doctest MTG
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
